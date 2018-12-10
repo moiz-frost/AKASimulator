e5e78a0005ee2e50e62bc59ab65d5618505a2026
Anas Ismail Khan <contact@anasismail.com>
2018-12-10 11:26:24 +0500

    Created a map for registers, updated the parser to use that map, added support for labels and implemented most of the functions.

--- src/com/company/AKAMips.java
+++ src/com/company/AKAMips.java
@@ -1,18 +1,47 @@
 package com.company;
 
 import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 // http://www.dsi.unive.it/~gasparetto/materials/MIPS_Instruction_Set.pdf
 public class AKAMips {
 
     int ram[] = new int[1000];
     int registers[] = new int[35];
+    Map<String,Integer> regMap;
     Parser parser;
     GUI gui;
 
     AKAMips(File file) {
         parser = new Parser(file);
         parser.machine = this;
+        regMap = new LinkedHashMap<String,Integer>();
+        regMap.put("$zero", 0);
+        regMap.put("$at", 1);
+        regMap.put("$v0", 2);
+        regMap.put("$v1", 3);
+        for (int i = 0; i < 4; i++) {
+            regMap.put("$a"+(i), i+4);
+        }
+        for (int i = 0; i < 8; i++) {
+            regMap.put("$a"+(i), i+8);
+        }
+        for (int i = 0; i < 8; i++) {
+            regMap.put("$s"+(i), i+16);
+        }
+        regMap.put("$a8", 24);
+        regMap.put("$a9", 25);
+        regMap.put("$k0", 26);
+        regMap.put("$k1", 27);
+        regMap.put("$gp", 28);
+        regMap.put("$sp", 29);
+        regMap.put("$fp", 30);
+        regMap.put("$ra", 31);
+        regMap.put("$pc", 32);
+        regMap.put("$hi", 33);
+        regMap.put("$lo", 34);
+        
     }
 
 }
--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -3,123 +3,229 @@ package com.company;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Parser {
+
     ArrayList<String> code;
     Scanner input;
     AKAMips machine;
+    Map<String, Integer> labels;
 
-    Parser(File file){
+    Parser(File file) {
         code = new ArrayList<String>();
+        labels = new LinkedHashMap<String, Integer>();
         try {
             input = new Scanner(file);
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found.");
         }
-        while (input.hasNext()){
-            code.add(input.nextLine());
+        while (input.hasNext()) {
+            String line = input.nextLine();
+            String[] split = line.split(":");
+            if (split.length > 1) {
+                labels.put(split[0], code.size());
+                line = split[1];
+            }
+            code.add(line);
         }
-
     }
 
-    void parseLine(int pc){
+    void parseLine(int pc) {
         String line = code.get(pc);
-        String[] tokens = line.split(" ");
-        switch (tokens[0]){
+        String[] tokens = line.split("[^\\w\\(\\)$]*");
+        String[] resplit;
+        String addr;
+        String offset;
+        int jumpTo;
+        switch (tokens[0]) {
             case "add":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])]+machine.registers[Integer.parseInt(tokens[3])];
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] + machine.registers[machine.regMap.get(tokens[3])];
                 break;
             case "sub":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])]-machine.registers[Integer.parseInt(tokens[3])];
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] - machine.registers[machine.regMap.get(tokens[3])];
                 break;
             case "addi":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])]+Integer.parseInt(tokens[3]);
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] + Integer.parseInt(tokens[3]);
                 break;
             case "addu":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])]+Integer.parseInt(tokens[3]);
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] + machine.registers[machine.regMap.get(tokens[3])]; // Not Supported
                 break;
             case "subu":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])]-Integer.parseInt(tokens[3]);
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] - machine.registers[machine.regMap.get(tokens[3])]; // Not Supported
                 break;
             case "addiu":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])]+Integer.parseInt(tokens[3]);
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] + machine.regMap.get(tokens[3]); // Not Supported
                 break;
             case "mul":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])]*machine.registers[Integer.parseInt(tokens[3])];
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] * machine.registers[machine.regMap.get(tokens[3])];
                 break;
             case "mult":
-                long product = machine.registers[Integer.parseInt(tokens[1])]*machine.registers[Integer.parseInt(tokens[2])];
+                long product = machine.registers[machine.regMap.get(tokens[1])] * machine.registers[machine.regMap.get(tokens[2])];
                 int hi = (int) (product >> 32);
                 int lo = (int) (product & 0xFFFFFFFF);
-                machine.registers[33]= hi;
-                machine.registers[34]= lo;
+                machine.registers[33] = hi;
+                machine.registers[34] = lo;
                 break;
             case "div":
-                machine.registers[33]= machine.registers[Integer.parseInt(tokens[1])]%machine.registers[Integer.parseInt(tokens[2])];
-                machine.registers[34]= machine.registers[Integer.parseInt(tokens[1])]/machine.registers[Integer.parseInt(tokens[2])];
+                machine.registers[33] = machine.registers[machine.regMap.get(tokens[1])] % machine.registers[machine.regMap.get(tokens[2])];
+                machine.registers[34] = machine.registers[machine.regMap.get(tokens[1])] / machine.registers[machine.regMap.get(tokens[2])];
                 break;
             case "and":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])] & machine.registers[Integer.parseInt(tokens[3])];
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] & machine.registers[machine.regMap.get(tokens[3])];
                 break;
             case "or":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])] | machine.registers[Integer.parseInt(tokens[3])];
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] | machine.registers[machine.regMap.get(tokens[3])];
                 break;
             case "andi":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])] & Integer.parseInt(tokens[3]);
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] & Integer.parseInt(tokens[3]);
                 break;
             case "ori":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])] | Integer.parseInt(tokens[3]);
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] | Integer.parseInt(tokens[3]);
                 break;
             case "sll":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])] << Integer.parseInt(tokens[3]);
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] << Integer.parseInt(tokens[3]);
                 break;
             case "srl":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])] >> Integer.parseInt(tokens[3]);
-                break;         
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])] >> Integer.parseInt(tokens[3]);
+                break;
             case "lw":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.ram[Integer.parseInt(tokens[3])+Integer.parseInt(tokens[2])];
+                resplit = tokens[2].split("\\(");
+                offset = resplit[0];
+                addr = resplit[1].replaceAll("\\)", "");
+                machine.registers[machine.regMap.get(tokens[1])] = machine.ram[Integer.parseInt(addr) + Integer.parseInt(offset)];
                 break;
             case "sw":
-                machine.ram[Integer.parseInt(tokens[3])+Integer.parseInt(tokens[2])]=machine.registers[Integer.parseInt(tokens[1])];
+                resplit = tokens[2].split("\\(");
+                offset = resplit[0];
+                addr = resplit[1].replaceAll("\\)", "");
+                machine.ram[Integer.parseInt(addr) + Integer.parseInt(offset)] = machine.registers[machine.regMap.get(tokens[1])];
                 break;
             case "lui":
-                machine.registers[Integer.parseInt(tokens[1])]=machine.registers[Integer.parseInt(tokens[2])]<<16;
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[Integer.parseInt(tokens[2])] << 16;
                 break;
             case "la":
-
+                code.set(pc, "lui $at, 4097");
+                code.add(pc + 1, "ori " + tokens[1] + ", $at, ");
+                machine.registers[32]--;
                 break;
             case "li":
+                machine.registers[machine.regMap.get(tokens[1])] = Integer.parseInt(tokens[2]);
                 break;
             case "mfhi":
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[33];
                 break;
             case "mflo":
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[34];
                 break;
             case "move":
+                machine.registers[machine.regMap.get(tokens[1])] = machine.registers[machine.regMap.get(tokens[2])];
                 break;
             case "beq":
+                if (machine.registers[machine.regMap.get(tokens[1])] == machine.registers[machine.regMap.get(tokens[2])]) {
+                    try {
+                        jumpTo = Integer.parseInt(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    } catch (NumberFormatException e) {
+                        jumpTo = labels.get(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    }
+                }
                 break;
             case "bne":
+                if (machine.registers[machine.regMap.get(tokens[1])] != machine.registers[machine.regMap.get(tokens[2])]) {
+                    try {
+                        jumpTo = Integer.parseInt(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    } catch (NumberFormatException e) {
+                        jumpTo = labels.get(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    }
+                }
                 break;
             case "bgt":
+                if (machine.registers[machine.regMap.get(tokens[1])] > machine.registers[machine.regMap.get(tokens[2])]) {
+                    try {
+                        jumpTo = Integer.parseInt(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    } catch (NumberFormatException e) {
+                        jumpTo = labels.get(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    }
+                }
                 break;
             case "bge":
+                if (machine.registers[machine.regMap.get(tokens[1])] >= machine.registers[machine.regMap.get(tokens[2])]) {
+                    try {
+                        jumpTo = Integer.parseInt(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    } catch (NumberFormatException e) {
+                        jumpTo = labels.get(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    }
+                }
                 break;
             case "blt":
+                if (machine.registers[machine.regMap.get(tokens[1])] < machine.registers[machine.regMap.get(tokens[2])]) {
+                    try {
+                        jumpTo = Integer.parseInt(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    } catch (NumberFormatException e) {
+                        jumpTo = labels.get(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    }
+                }
                 break;
             case "ble":
+                if (machine.registers[machine.regMap.get(tokens[1])] <= machine.registers[machine.regMap.get(tokens[2])]) {
+                    try {
+                        jumpTo = Integer.parseInt(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    } catch (NumberFormatException e) {
+                        jumpTo = labels.get(tokens[3]);
+                        machine.registers[32] = jumpTo;
+                    }
+                }
                 break;
             case "slt":
+                if (machine.registers[machine.regMap.get(tokens[2])] < machine.registers[machine.regMap.get(tokens[3])]) {
+                    machine.registers[machine.regMap.get(tokens[1])] = 1;
+                } else {
+                    machine.registers[machine.regMap.get(tokens[1])] = 0;
+                }
                 break;
             case "slti":
+                if (machine.registers[machine.regMap.get(tokens[2])] < Integer.parseInt(tokens[3])) {
+                    machine.registers[machine.regMap.get(tokens[1])] = 1;
+                } else {
+                    machine.registers[machine.regMap.get(tokens[1])] = 0;
+                }
                 break;
             case "j":
+                try {
+                    jumpTo = Integer.parseInt(tokens[1]);
+                    machine.registers[32] = jumpTo;
+                } catch (NumberFormatException e) {
+                    jumpTo = labels.get(tokens[1]);
+                    machine.registers[32] = jumpTo;
+                }
                 break;
             case "jr":
+                machine.registers[32] = machine.registers[machine.regMap.get(tokens[1])];
                 break;
             case "jal":
+                try {
+                    jumpTo = Integer.parseInt(tokens[1]);
+                    machine.registers[32] = jumpTo;
+                } catch (NumberFormatException e) {
+                    jumpTo = labels.get(tokens[1]);
+                    machine.registers[32] = jumpTo;
+                }
+                machine.registers[31]=pc;
                 break;
-            case "syscall":
+            case "syscall":              
                 break;
         }
     }
